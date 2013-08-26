c9f025c9b0be405406e08adf66d445d9100d701d
sureshprasanna70 <sureshprasanna70@gmail.com>
2013-08-26 22:22:37 +0530

    added delete to fromtan.c

--- fromtan.c
+++ fromtan.c
@@ -42,16 +42,20 @@ void add_between(int a1,int b)
 	}
 	add(b);
 }
-void del_node(int a)
+void del_node()
 {
-	nodes *temp;
+	nodes *temp,*last_prev;
 	temp=head;
 	printf("Second node:%d",temp->next->no);
-	while(temp->next->no!=a)
+	while(temp->next!=NULL)
 	{
+		last_prev=temp;
 		printf("%d",temp->no);
 		temp=temp->next;
 	}
+	last_prev->next=NULL;
+	free(temp);
+
 }
 void show_list()
 {
@@ -65,10 +69,17 @@ void show_list()
 }
 int main()
 {
-	int i=0,bw=0;
+	int i=0,bw=0,n=0,t=0;
 	printf("Enter a value to create link list");
 	scanf("%d",&i);
 	add(i);
+	printf("No of elements to be inserted:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{	
+		scanf("%d",&t);
+		add(t);
+	}
 	printf("Enter value to be added the list and number after which the the value\nshould be inserted seperated by newline characater");
 	scanf("%d\n%d",&bw,&i);
 	add_between(i,bw);
